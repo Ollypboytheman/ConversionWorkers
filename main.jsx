4d148c1d7e0cf5c98d03ebce8c830bf73c527b8a
Ollypboytheman <olly@radio-controlled.co.uk>
2025-12-07 21:34:29 +0000

    Update import path for ConversionKVAdmin component

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ConversionKVAdmin from "./ConversionKVAdmin.jsx";
+import ConversionKVAdmin from "./src/ConversionKVAdmin.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
