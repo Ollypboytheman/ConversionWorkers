a8309ce1eb3a04dcc23c97e3b55ace10954dc99b
Ollypboytheman <olly@radio-controlled.co.uk>
2025-12-08 21:03:58 +0000

    Delete src/functions/api directory

--- src/functions/api/save-experiments.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestPut(context) {
-  const key = context.request.url.split("?key=")[1];
-  const data = await context.request.json();
-
-  await context.env.EXPERIMENTS_KV.put(key, JSON.stringify(data));
-
-  return new Response("Saved", { status: 200 });
-}
