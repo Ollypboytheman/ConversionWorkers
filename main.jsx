22f59b1301b12f19938d9cee39a4b0e39aea5d0c
Ollypboytheman <olly@radio-controlled.co.uk>
2025-12-10 21:40:03 +0000

    Add Tailwind CSS import to main.jsx

--- main.jsx
+++ main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import ConversionKVAdmin from "./src/ConversionKVAdmin.jsx";
 
+// ✅ Tailwind CSS import – make sure this is the first import
+import "./index.css";
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ConversionKVAdmin />
